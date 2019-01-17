69cd0193f456d20cf8ce20d1702bb888739f97c2
ses8104 <ses8104@naver.com>
2019-01-17 14:51:55 +0900

    first commit

---
+++ MyMenu1/MyTestMenu1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ImageToWriteTEXT
+{
+    partial class ImageToWriteTEXT
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCreateImage = new System.Windows.Forms.Button();
+            this.txt_etc = new System.Windows.Forms.RichTextBox();
+            this.txt_mold = new System.Windows.Forms.TextBox();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_item = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblItemName = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.imageForm1 = new ImageForm.ImageForm();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(335, 517);
+            this.panel1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.btnCreateImage);
+            this.groupBox1.Controls.Add(this.txt_etc);
+            this.groupBox1.Controls.Add(this.txt_mold);
+            this.groupBox1.Controls.Add(this.txt_user);
+            this.groupBox1.Controls.Add(this.txt_item);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblItemName);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 512);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "등록";
+            // 
+            // btnCreateImage
+            // 
+            this.btnCreateImage.Location = new System.Drawing.Point(245, 99);
+            this.btnCreateImage.Name = "btnCreateImage";
+            this.btnCreateImage.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateImage.TabIndex = 7;
+            this.btnCreateImage.Text = "이미지생성";
+            this.btnCreateImage.UseVisualStyleBackColor = true;
+            this.btnCreateImage.Click += new System.EventHandler(this.btnCreateImage_Click);
+            // 
+            // txt_etc
+            // 
+            this.txt_etc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_etc.Location = new System.Drawing.Point(7, 125);
+            this.txt_etc.Name = "txt_etc";
+            this.txt_etc.Size = new System.Drawing.Size(313, 381);
+            this.txt_etc.TabIndex = 6;
+            this.txt_etc.Text = "";
+            // 
+            // txt_mold
+            // 
+            this.txt_mold.Location = new System.Drawing.Point(75, 75);
+            this.txt_mold.Name = "txt_mold";
+            this.txt_mold.Size = new System.Drawing.Size(245, 21);
+            this.txt_mold.TabIndex = 5;
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(75, 49);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(245, 21);
+            this.txt_user.TabIndex = 5;
+            // 
+            // txt_item
+            // 
+            this.txt_item.Location = new System.Drawing.Point(75, 23);
+            this.txt_item.Name = "txt_item";
+            this.txt_item.Size = new System.Drawing.Size(245, 21);
+            this.txt_item.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(7, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "기타";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(7, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "금형코드";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(7, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "담당자";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblItemName
+            // 
+            this.lblItemName.BackColor = System.Drawing.SystemColors.Control;
+            this.lblItemName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblItemName.Location = new System.Drawing.Point(7, 21);
+            this.lblItemName.Name = "lblItemName";
+            this.lblItemName.Size = new System.Drawing.Size(65, 23);
+            this.lblItemName.TabIndex = 0;
+            this.lblItemName.Text = "제품";
+            this.lblItemName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.Controls.Add(this.imageForm1);
+            this.panel2.Location = new System.Drawing.Point(341, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(468, 517);
+            this.panel2.TabIndex = 2;
+            // 
+            // imageForm1
+            // 
+            this.imageForm1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imageForm1.BackColor = System.Drawing.SystemColors.Control;
+            this.imageForm1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageForm1.Location = new System.Drawing.Point(3, 3);
+            this.imageForm1.Name = "imageForm1";
+            this.imageForm1.Size = new System.Drawing.Size(474, 510);
+            this.imageForm1.TabIndex = 1;
+            // 
+            // MyTestMenu1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(810, 525);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "MyTestMenu1";
+            this.Text = "문서이미지변환";
+            this.Load += new System.EventHandler(this.MyMenu1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCreateImage;
+        private System.Windows.Forms.RichTextBox txt_etc;
+        private System.Windows.Forms.TextBox txt_mold;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_item;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblItemName;
+        private System.Windows.Forms.Panel panel2;
+        private ImageForm.ImageForm imageForm1;
+    }
+}
+
---
+++ MyMenu1/MyTestMenu1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ImageToWriteTEXT
+{
+    public partial class ImageToWriteTEXT : Form
+    {
+        string _ImgPath = @"C:\Users\esseo\Documents\Visual Studio 2012\Projects\MyMenu1\MyMenu1\bin\Debug\ImageForm.jpg";
+        string _XmlPath = @"C:\Users\esseo\Documents\Visual Studio 2012\Projects\MyMenu1\MyMenu1\bin\Debug\ImagePoint.xml";
+
+        public ImageToWriteTEXT()
+        {
+            InitializeComponent();
+            
+            SetStyle(ControlStyles.DoubleBuffer, true);
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
+        }
+ 
+        private void MyMenu1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                imageForm1.ReadImage(_ImgPath, _XmlPath); //이미지파일, XML파일 읽어온다.
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void btnCreateImage_Click(object sender, EventArgs e)//생성버튼
+        {
+            try
+            {
+                imageForm1.WriteImage(_ImgPath); //이미지 파일을 신규로 생성
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
